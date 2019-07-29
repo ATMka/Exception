9616b8dba6342d2921e709657bb6bb3eb11d13fc
atmka <corp.dvd@gmail.com>
2019-07-29 22:44:28 +0400

    Добавлен класс исключений

---
+++ ReadingFile/src/com/epam/file/ExceptionsForThisProject.java
@@ -0,0 +1,7 @@
+package com.epam.file;
+
+public class ExceptionsForThisProject extends Exception {
+    public ExceptionsForThisProject(String message) {
+        super(message);
+    }
+}
--- ReadingFile/src/com/epam/file/MainReader.java
+++ ReadingFile/src/com/epam/file/MainReader.java
@@ -1,7 +1,10 @@
 package com.epam.file;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -26,31 +29,46 @@ public class MainReader {
      * @param args
      */
     public static void main(String[] args) {
-        readFile();
+        try {
+            readFile();
+        }catch (ExceptionsForThisProject e){
+            System.out.println("Моё исключение выдало вот такую забавную ошибку: " + e.getMessage());
+        }
+
     }
 
     /**
      * Метод для чтения дат из файла
      */
-    public static void readFile() {
+    public static void readFile() throws ExceptionsForThisProject {
         //Открываем потоки на чтение из файла
-        FileReader reader = new FileReader("file.txt");
-        BufferedReader byfReader = new BufferedReader(reader);
+        try(FileReader reader = new FileReader("file.txt");){
+            BufferedReader byfReader = new BufferedReader(reader);
+            //Читаем первую строку из файла
+            String strDate = byfReader.readLine();
+            while(strDate != null) {
+                try{
+                    //Преобразуем строку в дату
+                    Date date = parseDate(strDate);
 
-        //Читаем первую строку из файла
-        String strDate = byfReader.readLine();
+                    //Выводим дату в консоль в формате dd-mm-yy
+                    System.out.printf("%1$td-%1$tm-%1$ty \n", date);
 
-        while(strDate != null) {
-            //Преобразуем строку в дату
-            Date date = parseDate(strDate);
+                }catch (ParseException e){
+                    System.out.println("Неверный формат даты. Текст ошибки: " + e.getMessage());
+                }
 
-            //Выводим дату в консоль в формате dd-mm-yy
-            System.out.printf("%1$td-%1$tm-%1$ty \n", date);
+                //Читаем следующую строку из файла
+                strDate = byfReader.readLine();
 
-            //Читаем следующую строку из файла
-            strDate = byfReader.readLine();
-
+            }
+        }catch(IOException e) {
+            throw new ExceptionsForThisProject("Не могу найти файл. Может его там и нет вовсе.");
         }
+
+
+
+
     }
 
     /**
@@ -58,7 +76,7 @@ public class MainReader {
      * @param strDate строковое представление даты
      * @return
      */
-    public static Date parseDate(String strDate) {
+    public static Date parseDate(String strDate) throws ParseException{
         return dateFormatter.parse(strDate);
     }
 }
